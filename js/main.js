e1a200890c9e81919a0123f22fd5996d98e8977b
KuzMich1994 <drumer19940310@mail.ru>
2021-03-24 17:29:23 +0300

    Create: added function showGoods

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ const showModalCart = () => {
 
 		if (target.closest('.button-cart')) {
 			modalCart.classList.add('show');
-			document.body.style.cssText = `overflow-y: hidden; padding-right: 18px;`;
+			document.body.classList.add('scroll-hidden');
 		}
 		if (target.matches('.overlay') || target.matches('.modal-close')) {
 			modalCart.classList.remove('show');
-			document.body.style.cssText = `overflow-y: visible; padding-right: 0;`;
+			document.body.classList.remove('scroll-hidden');
 		}
 	});
 };
@@ -41,9 +41,11 @@ const scrollToAnchors = () => {
 			let target = e.target;
 			if (target.closest('[href*="#"]')) {
 				target = target.closest('[href*="#"]');
-				console.log(target);
-				let w = window.pageYOffset - 1;
+				let w = window.pageYOffset;
 				let hash = target.href.replace(/[^#]*(.*)/, '$1');
+				if (hash === '#') {
+					return;
+				}
 				let t = document.querySelector(hash).getBoundingClientRect().top;
 				let start = null;
 				const step = time => {
@@ -53,10 +55,8 @@ const scrollToAnchors = () => {
 					let progress = time - start;
 					let r = (t < 0 ? Math.max(w - progress / v, v + t) : Math.min(w + progress / v, w + t));
 					if (r < -100) {
-						console.log(r);
 						return;
 					}
-					console.log(r);
 					window.scrollTo(0, r);
 
 					if (r !== w + t) {
@@ -73,6 +73,89 @@ const scrollToAnchors = () => {
 };
 
 scrollToAnchors();
+
+const showGoods = () => {
+	const more = document.querySelector('.more');
+	const navigation = document.querySelector('.navigation');
+	const longGoodsList = document.querySelector('.long-goods-list');
+
+	const getGoods = async () => {
+		const result = await fetch('db/db.json');
+
+		if (!result.ok) {
+			throw new Error(`Error: ${result.status}`);
+		}
+		return await result.json();
+	};
+
+	const createCard = objCard => {
+		const newCard = document.createElement('div');
+		newCard.className = 'col-lg-3 col-sm-6';
+
+		newCard.innerHTML = `
+			<div class="goods-card">
+				${objCard.label ? 
+					`<span class="label">${objCard.label}</span>` :
+					''}
+				<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+				<h3 class="goods-title">${objCard.name}</h3>
+				<p class="goods-description">${objCard.description}</p>
+				<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+					<span class="button-price">$${objCard.price}</span>
+				</button>
+			</div>
+		`
+		return newCard;
+	};
+
+	const renderCards = data => {
+		longGoodsList.textContent = '';
+		const cards = data.map(createCard);
+		longGoodsList.append(...cards);
+
+		document.body.classList.add('show-goods');
+	};
+
+	const filterCards = (filed, value) => {
+		getGoods()
+			.then((data) => {
+				const filteredGoods = data.filter((good) => {
+					return good[filed] === value;
+				});
+				return filteredGoods;
+			})
+			.then(renderCards);
+	};
+
+	document.addEventListener('click', e => {
+		const target = e.target;
+
+		if (target.matches('.navigation-link')) {
+			e.preventDefault();
+			filterCards(target.dataset.field, target.textContent);
+		}
+		if (target.textContent === 'All' || target.matches('.more')) {
+			e.preventDefault();
+			getGoods().then(renderCards);
+		}
+		if (target.closest('.accessories')) {
+			filterCards('category', 'Accessories');
+			document.getElementById('body').scrollIntoView({
+				block: 'start',
+				behavior: 'smooth'
+			})
+		}
+		if (target.closest('.clothing')) {
+			filterCards('category', 'Clothing');
+			document.getElementById('body').scrollIntoView({
+				block: 'start',
+				behavior: 'smooth'
+			})
+		}
+	});
+};
+
+showGoods();
 
 
 
